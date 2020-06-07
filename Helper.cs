b6a3e372c80c89ffd2294268e56d9456d906c111
LoganJM <logan.mospens@gmail.com>
2020-06-07 18:32:01 -0400

    removing unused imports

--- Helper.cs
+++ Helper.cs
@@ -1,11 +1,7 @@
-using System;
-using System.Collections.Generic;
+﻿using System;
 using System.Diagnostics;
 using System.IO;
-using System.Linq;
 using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Xml;
 
